05e1620eff5f8fc7647bf2f718546217b4df2934
Louis-7 <louisgh.cn@gmail.com>
2017-12-11 17:55:01 +0800

    [update] Add footer and re-structure css.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { ScrollBoardComponent } from './scroll-board/scroll-board.component';
-import { NavComponent } from './nav/nav.component';
-
 // import angular material components
 import { OwnAngularMaterialModule } from '../shared/own-angular-material/own-angular-material.module';
 
+import { ScrollBoardComponent } from './scroll-board/scroll-board.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
     OwnAngularMaterialModule
   ],
-  declarations: [ScrollBoardComponent, NavComponent],
-  exports: [ScrollBoardComponent, NavComponent],
+  declarations: [ScrollBoardComponent, NavComponent, FooterComponent],
+  exports: [ScrollBoardComponent, NavComponent, FooterComponent],
 })
 export class CoreModule { }
